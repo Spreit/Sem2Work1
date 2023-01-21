11cc499df7bae787df40505251216a65f15af3fd
Spreit <vladislav.dombrovskii@gmail.com>
2023-01-21 14:56:47 +0200

    works now

--- Work1.cpp
+++ Work1.cpp
@@ -1,4 +1,4 @@
-﻿// Work1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.\
+// Work1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.\
 // 
 /* «Шифрование»
 
@@ -30,45 +30,212 @@ my Ivan.is Hello, name
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
 #include <iterator>
+#include <algorithm>
+#include <functional>
 
 std::string message = "Hello, my name is Ivan.";
 
 class encryption {
 private:
     std::vector<std::string> m_words;
-    std::string * m_message;
+    std::vector<int> key_index;
+    std::vector<int> key_full;
+    std::vector<int> key_encode;
     int word_count;
-    std::vector<int> key;
 
-    std::string split_by_spaces(const std::string message) {
-        
-    }
+private:
+    void split_by_spaces(const std::string &message) {
+        int mes_size = message.size();
+        std::string word;
+        int word_begin = 0;
+        int word_len = 0;
 
-    template <typename Out>
-    void split(const std::string& s, char delim, Out result) {
-        std::istringstream iss(s);
-        std::string item;
-        while (std::getline(iss, item, delim)) {
-            *result++ = item;
+        for (int i = 0; i < mes_size; i++) {
+            if (message[i] == ' ') {
+                m_words.emplace_back(message.substr(word_begin, word_len + 1));
+                word_len = 0;
+                word_begin = i + 1;
+            }else {
+                word_len++;
+            }
+
+            if (i == mes_size - 1) {
+                m_words.emplace_back(message.substr(word_begin, word_len));
+            }
         }
     }
-    std::vector<std::string> split(const std::string& s, char delim) {
-        std::vector<std::string> elems;
-        split(s, delim, std::back_inserter(elems));
-        return elems;
+
+    int Fib(int i) {
+        if (i < 1) return 0;
+        if (i == 1) return 1;
+return Fib(i - 1) + Fib(i - 2);
     }
-public:
-    encryption(std::string message) {
+
+    int Fib_to_index(int fibnum) {
+        if (fibnum == 0) {
+            return 0;
+        }
+        else if (fibnum == 1) {
+            return 1;
+        }
+        else {
+            int nres = 0;
+            int i = 2;
+            while (nres < fibnum) {
+                nres = Fib(i);
+                i++;
+                if (nres == fibnum) {
+                    return i - 2;
+                };
+                if (nres > fibnum) {
+                    return -1;
+                };
+            };
+        };
 
     }
+
+    void key_to_index() {
+        int ind;
+        for (int i = 0; i < word_count; i++) {
+            ind = Fib_to_index(key_index[i]);
+            if (ind >= 0) {
+                key_index[i] = ind;
+            }
+            else {
+                std::cout << "Invalid fib sequence." << std::endl;
+                remove_key();
+                break;
+            }
+        }
+
+    }
+
+    void check_seq(std::vector<int> key) {
+        std::vector<int> temp_key = key;
+        std::sort(temp_key.begin(), temp_key.end());
+
+        for (int i = 0; i < temp_key.size() - 1; i++) {
+            if (temp_key[i] == temp_key[i + 1]) {
+                std::cout << "Invalid fib sequence." << std::endl;
+                remove_key();
+            }
+        }
+    }
+
+    void encrypt(std::vector<int> index_key) {
+
+        std::vector<std::string> m_cr;
+
+        for (int i = 0; i < word_count; i++) {
+            m_cr.emplace_back(m_words[index_key[i]]);
+        }
+        m_words = m_cr;
+
+    };
+
+    void decrypt(std::vector<int> index_key) {
+
+        std::vector<std::string> m_cr;
+
+        for (int i = 0; i < word_count; i++) {
+            m_cr.emplace_back(m_words[index_key[i]]);
+        }
+        m_words = m_cr;
+    }
+
+    void print_message() {
+        for (int i = 0; i < word_count; i++) {
+            std::cout << m_words[i];
+        }
+        std::cout << std::endl;
+    };
+
+public:
+    encryption(const std::string &message) {
+        split_by_spaces(message);
+        word_count = m_words.size();
+    };
+
+    void input_key(std::vector<int> inserted_key) {
+        if (key_index.size() != 0) {
+            std::cout << "Key is already inserted. Remove key to insert." << std::endl;
+        }
+        else {
+
+            std::cout << "Key inserted." << std::endl;
+
+            if (inserted_key.size() != word_count) {
+                std::cout << "Incorrect key size!" << std::endl;
+                remove_key();
+            }
+            else {
+                key_full  = inserted_key;
+                key_index = inserted_key;
+                key_to_index();
+            }
+
+            //Correct sequence check
+            if (key_index.size() != 0){
+                check_seq(key_index);
+            }
+
+        }
+
+    }
+
+    void remove_key() {
+        key_full.erase(key_full.begin(), key_full.end());
+        key_index.erase(key_index.begin(), key_index.end());
+        std::cout << "Key removed." << std::endl;
+    }
+
+    void encode() {
+
+        if (key_index.size() == 0) {
+            std::cout << "Key is missing!" << std::endl;
+        }
+        else {
+            encrypt(key_index);
+            key_encode = key_full;
+            print_message();
+        }
+    }
+    
+    void decode() {
+
+        if (key_index.size() == 0) {
+            std::cout << "Key is missing!" << std::endl;
+        }
+        else if(key_full != key_encode){
+            std::cout << "Wrong key!!" << std::endl;
+        }
+        else {
+            decrypt(key_index);
+            print_message();
+        }
+    }
+
+
 
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> new_key = { 5,3,2,1,0 };
+    encryption masage(message);
+
+    std::cout << new_key.size() << std::endl;
+    masage.input_key(new_key);
+    masage.encode();
+
+//    masage.remove_key();
+//    new_key = { 5,1,2,3,0 };
+//    masage.input_key(new_key);
+    
+    masage.decode();
+    masage.remove_key();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
